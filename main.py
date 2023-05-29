a7410d65a520be9054a28662b8b928e1cb0dd96d
trix0831 <joseph831hsu@gmail.com>
2023-05-29 21:23:43 +0800

    main.py update

--- main.py
+++ main.py
@@ -27,10 +27,10 @@ screen.blit(image_1,(0,0))
 
 buttons = []
 
-PVE_button = BT.button(button_width, button_height, (400, 350), ".\image\pve_black.jpg", ".\image\pve_black.jpg", pve.pve)
+PVE_button = BT.button(button_width, button_height, (400, 350), ".\image\pve_black.jpg", ".\image\pve_down.png", True, pve.pve)
 buttons.append(PVE_button)
 
-PVP_button = BT.button(button_width, button_height, (640, 350), ".\image\pvp_black.jpg", ".\image\pvp_black.jpg", pvp.pvp)
+PVP_button = BT.button(button_width, button_height, (640, 350), ".\image\pvp_black.jpg", ".\image\pvp_down.png", True, pvp.pvp)
 buttons.append(PVP_button)
 
 print(buttons)
@@ -40,30 +40,30 @@ for button in buttons:
 
 running = True
 
+excute_index = -1
+
 while running:
     mouse_x, mouse_y = pg.mouse.get_pos()
-        
-    for button in buttons:
-        if button.pos[0] < mouse_x < button.pos[0] + button.width and button.pos[1] < mouse_y < button.pos[1] + button.height:
-            screen.blit(button.button_image[0], button.pos)
-            
-        else:
-            screen.blit(button.button_image[0], button.pos)
     
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
             
-        elif event.type == pg.MOUSEBUTTONDOWN:
-            for button in buttons:
-                if button.pos[0] < mouse_x < button.pos[0] + button.width and button.pos[1] < mouse_y < button.pos[1] + button.height:
-                    screen.blit(button.button_image[1], button.pos)
-                    button.func()
-                    
-    for button in buttons:
-        screen.blit(button.button_image[0], button.pos)
+        for button in buttons:
+            if button.pos[0] < mouse_x < button.pos[0] + button.width and button.pos[1] < mouse_y < button.pos[1] + button.height:
+                screen.blit(button.button_image[1], button.pos)
+            else:
+                screen.blit(button.button_image[0], button.pos)
+            
+        if event.type == pg.MOUSEBUTTONUP:
+            for button_index in range(len(buttons)):
+                if buttons[button_index].pos[0] < mouse_x < buttons[button_index].pos[0] + buttons[button_index].width and buttons[button_index].pos[1] < mouse_y < buttons[button_index].pos[1] + buttons[button_index].height:
+                    excute_index = button_index
+                    running = False
                 
         
     pg.display.update()
             
 pg.quit()   
+
+buttons[excute_index].func()
